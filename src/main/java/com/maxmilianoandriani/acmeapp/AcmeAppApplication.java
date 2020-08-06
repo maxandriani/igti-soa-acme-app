f3b51bf95d0069f13050335d70eee48ca14586ed
Maxmiliano Reipert Andriani <max.andriani@becomex.com.br>
2020-08-06 09:54:14 -0300

    Setup project...

---
+++ src/main/java/com/maxmilianoandriani/acmeapp/AcmeAppApplication.java
@@ -0,0 +1,13 @@
+package com.maxmilianoandriani.acmeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxmilianoandriani/acmeapp/AcmeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxmilianoandriani.acmeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
